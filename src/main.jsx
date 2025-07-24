9e85998d0283bf8eb333e1a0c45a257d77f3540c
Kalvin <kalvinsupriadi3@gmail.com>
2025-07-24 15:54:41 +0700

    Add Pages Folder

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App.jsx";
+// src/App.jsx
+import { Routes, Route } from "react-router-dom"
+import Login from "./pages/Login"
 
-createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-);
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<h1>Home Page</h1>} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  )
+}
+
+export default App
