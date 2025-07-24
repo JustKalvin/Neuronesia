a971adbe37ea0e96987901ce185e234d2dfb04b9
juanariviano <juanariviano@gmail.com>
2025-07-25 06:08:30 +0700

    Update Landing.jsx

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "../style/style.css";
-import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";       
 import Navbar from "./Navbar";
 import Card from "./Card";
 import users from "../assets/user/user.png";
