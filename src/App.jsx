96822f37b15e3b9b55932badedc1189a0eb676a9
juanariviano <juanariviano@gmail.com>
2025-07-24 16:05:22 +0700

    fix-2

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,22 @@ import viteLogo from '/vite.svg'
 import './style/style.css'
 import { Routes, Route } from "react-router-dom";
 import Landing from './components/Landing'
+<<<<<<< Updated upstream
 import Home from './pages/Home';
 import Login from './pages/login';
+=======
+import Chatbot from './pages/Chatbot';
+>>>>>>> Stashed changes
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Landing />} />
+<<<<<<< Updated upstream
       <Route path="/login" element={<Login />} />
+=======
+      <Route path="/chatbot" element={<Chatbot/>} />
+>>>>>>> Stashed changes
     </Routes>
   )
 }
--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -9,7 +9,7 @@ const Landing = () => {
       <p className="w-[300px] text-center">
         Personalized Business Advisor
       </p>
-      <Link to="/home" className="bg-red-100 px-4 py-2 rounded-md mt-4">Get Started</Link>
+      <Link to="/chatbot" className="bg-red-100 px-4 py-2 rounded-md mt-4">Get Started</Link>
     </div>
   );
 };
