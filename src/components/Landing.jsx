3daa52d9fa63bb46f557521bd4cff61e9255997d
Sprickels <108860809+Sprickels@users.noreply.github.com>
2025-07-24 18:15:17 +0700

    Input custom font and colour

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -23,8 +23,8 @@ const Landing = () => {
   }, []);
 
   return (
-    <div className="bg-[#DEE4E2] flex flex-col h-[100vh] justify-center items-center">
-      <h1 className="font-bold text-[25px]">ADVISOR</h1>
+    <div className="bg-lightGreen flex flex-col h-[100vh] justify-center items-center">
+      <h1 className="font-Montserrat font-bold text-[25px]">ADVISOR</h1>
       <p className="w-[300px] text-center">Personalized Business Advisor</p>
 
       <Link
