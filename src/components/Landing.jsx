47362b4d63eb320b3e2eb74ed180038062556e80
juanariviano <juanariviano@gmail.com>
2025-07-24 16:15:46 +0700

    testing-login

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,15 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import "../style/style.css";
 import { Link } from "react-router-dom";
 
 const Landing = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
     <div className="flex flex-col h-[100vh] justify-center items-center">
       <h1 className="font-bold text-[25px]">Neuronesia</h1>
-      <p className="w-[300px] text-center">
-        Personalized Business Advisor
-      </p>
-      <Link to="/chatbot" className="bg-red-100 px-4 py-2 rounded-md mt-4">Get Started</Link>
+      <p className="w-[300px] text-center">Personalized Business Advisor</p>
+
+      <Link
+        to={isLoggedIn ? "/chatbot" : "/login"}
+        className="bg-blue-500 text-white px-4 py-2 rounded-md mt-4 inline-block"
+      >
+        Get Started
+      </Link>
     </div>
   );
 };
